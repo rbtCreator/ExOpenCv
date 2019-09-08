35afafc664956ff0e5ada0aa86c4584142b91cad
TrihlebDV <trihlebdv@gmail.com>
2019-09-08 12:11:43 +0300

    add exercise 3 to capter5 O

---
+++ capter5/ex4/main.cpp
@@ -0,0 +1,25 @@
+#include "opencv2/opencv.hpp"
+#include <string>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    cv::Mat img = cv::Mat(cv::Size(210, 210), CV_8UC1, cv::Scalar(0));
+    cv::String window = cv::String("window");
+    cv::namedWindow(window, cv::WINDOW_AUTOSIZE);
+    cv::imshow(window, img);
+    cv::waitKey(0);
+    for(int i = 210; i > 0; i -= 10)
+    {
+        cv::Rect ROI = cv::Rect(0, 0, i, i);
+        cv::Mat img_roi = img(ROI);
+        img_roi.setTo(cv::Scalar(i));
+    }
+    cv::imshow(window, img);
+    cv::waitKey(0);
+    cv::destroyWindow(window);
+    return 0;
+}
+
+
+
